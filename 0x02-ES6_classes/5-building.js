e3c07df303c856bcb4890387caae313db13c4242
IBruteDude <kamarf505@gmail.com>
2024-06-04 17:57:33 +0300

    Added most ES6 classes project tasks

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor.name !== 'Building' && !this.evacuationWarningMessage) throw new Error('Class extending Building must override evacuationWarningMessage');
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}
--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,30 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+}
